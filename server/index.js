c4879c7e9464ae1963f0b4114a727bc5110b6a7f
Akshay Sahu <akki5233@gmail.com>
2021-04-23 16:37:17 +0530

    Model and Controllers folder

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import postRoutes from './routes/posts.js';
 const PORT = process.env.PORT || 5123;
 const app = express();
 
@@ -9,6 +9,10 @@ app.use(express.json({ limit: '30mb', extended: true }));
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
+// Routes
+app.use('/posts',postRoutes);
+
+//MongoDB connection
 const MongoURI = "mongodb+srv://akshay:admin@cluster0.3sl2w.mongodb.net/post_it?retryWrites=true&w=majority" ;
 
 mongoose.connect(MongoURI,{useNewUrlParser:true, useUnifiedTopology:true}, (err,connection)=>{
@@ -22,3 +26,5 @@ mongoose.connect(MongoURI,{useNewUrlParser:true, useUnifiedTopology:true}, (err,
         console.log("Error in DB connection", err.message);
     }
 })
+
+mongoose.set('useFindAndModify', false);

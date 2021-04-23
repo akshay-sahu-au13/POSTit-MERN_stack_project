d41f6701901d1b5254fec56ae6043869bd63f48b
Akshay Sahu <akki5233@gmail.com>
2021-04-23 16:59:57 +0530

    Added post route, now createing front end for the same

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,4 +1,19 @@
+import PostMessage from '../Model/postMessage.js';
+export const getPosts = async (req, res) => {
+    try {
+        const posts = await PostMessage.find();
+        res.status(200).send(posts);
+    } catch (error) {
+        res.status(404).send({message:error.message});
+    }
+}
 
-export const getPosts = (req, res) => {
-    res.send("It works");
+export const createPosts = async (req, res) => {
+    const post= req.body;
+    try {
+        const new_post = new PostMessage(post);
+        await new_post.save();
+    } catch (error) {
+        res.status(409).send({message:error.message});
+    }
 }

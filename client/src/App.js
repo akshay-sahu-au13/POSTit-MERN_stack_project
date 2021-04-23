78e0242594d20346683fd17b7c261714eac855ae
Akshay Sahu <akki5233@gmail.com>
2021-04-23 21:45:28 +0530

    Gonna take a break and continue the rest of the coding later

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Container, AppBar, Typography, Grid, Grow } from '@material-ui/core';
+import {useDispatch} from 'react-redux';
+
+import {getPosts} from './actions/posts'
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 import memories from './images/memories.png';
 import useStyles from './styles';
 export default function App() {
     const classes = useStyles();
+    const dispatch = useDispatch();
+
+    const useEffect(() => {
+        dispatch(getPosts())
+        
+    }, [dispatch])
+
     return (
         <Container maxidth="lg">
             <AppBar className={classes.appBar} position="static"  color="inherit">

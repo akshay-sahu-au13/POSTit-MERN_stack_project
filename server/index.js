c9b23d216ba7567bbe93041378f992c6b25904e7
Akshay Sahu <akki5233@gmail.com>
2021-04-23 16:13:06 +0530

    Initial commit - DB connection and port activation

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const PORT = process.env.PORT || 5123;
+const app = express();
+
+app.use(express.json({ limit: '30mb', extended: true }));
+app.use(express.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+const MongoURI = "mongodb+srv://akshay:admin@cluster0.3sl2w.mongodb.net/post_it?retryWrites=true&w=majority" ;
+
+mongoose.connect(MongoURI,{useNewUrlParser:true, useUnifiedTopology:true}, (err,connection)=>{
+    if (connection){
+        console.log("DB Connected!");
+        app.listen(PORT, ()=>{
+            console.log(`Listening to http://localhost:${PORT}`);
+        })
+    }
+    if (err){
+        console.log("Error in DB connection", err.message);
+    }
+})
